fb94ccb842cd74f676fab881d4098b9858dbb370
red-pencil@alienware <hx.4700.00@gmail.com>
2019-04-20 20:15:35 +1200

    update

---
+++ Assets/gCameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gCameraController : MonoBehaviour
+{
+    GameObject gbCamera;
+
+    public float sensitivityX = 15F;
+    public float sensitivityY = 15F;
+    public float minimumX = -360F;
+    public float maximumX = 360F;
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+    private float rotationX = 0F;
+    private float rotationY = 0F;
+
+    void Start()
+    {
+        gbCamera = GameObject.Find("playerHead").transform.Find("Main Camera").gameObject;
+    }
+
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/gPlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gPlayerController : MonoBehaviour
+{
+    GameObject gbCamera;
+    GameObject gbBody;
+
+    public float sensitivityX = 15F;
+    public float sensitivityY = 15F;
+    public float minimumY = -90F;
+    public float maximumY = 90F;
+    public float minimumH = -60F;
+    public float maximumH = 60F;
+    public float rotationV = 0F;
+    public float rotationH = 0F;
+
+
+    Quaternion orginalTrans;
+
+    void Start()
+    {
+        gbCamera = Camera.main.gameObject;
+
+        gbBody = gameObject.transform.Find("dummyBody").gameObject;
+
+        orginalTrans = transform.localRotation;
+    }
+    
+
+    void Update()
+    {
+        Quaternion hQu, vQu;
+
+
+
+        //rotationV = gbCamera.transform.rotation.eulerAngles.x;
+        //rotationH = gbCamera.transform.rotation.eulerAngles.y;
+
+        //rotationH = gbCamera.transform.rotation.eulerAngles.y - gbBody.transform.localRotation.eulerAngles.y;
+
+
+        rotationH = gbCamera.transform.rotation.eulerAngles.y - gbBody.transform.localRotation.eulerAngles.y;
+
+        hQu = Quaternion.AngleAxis(rotationH, Vector3.up);
+        vQu = Quaternion.AngleAxis(rotationV, Vector3.right);
+
+        gbBody.transform.localRotation = gbBody.transform.localRotation * hQu * vQu;
+
+    }
+}
