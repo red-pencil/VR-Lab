8358aad89f6d352fbf8b4b23ad2ea9c47143339c
red-pencil@alienware <hx.4700.00@gmail.com>
2019-04-26 15:29:58 +1200

    update

--- Assets/Scripts/GUIController.cs
+++ Assets/Scripts/GUIController.cs
@@ -8,7 +8,7 @@ public class GUIController : MonoBehaviour
 
     public Text message;
     public GameObject menu;
-    public GameObject PUI, bgm, map, cam;
+    public GameObject PUI, bgm, map, cam, thirdCam;
     ButtonController buttonCtrl;
 
     void Start()
@@ -19,6 +19,7 @@ public class GUIController : MonoBehaviour
         PUI = GameObject.Find("PlainUI");
         map = PUI.transform.Find("Map").gameObject;
         cam = PUI.transform.Find("DeviceCamera").gameObject;
+        thirdCam = PUI.transform.Find("ThirdPerson").gameObject;
         buttonCtrl = GameObject.Find("Player").GetComponent<ButtonController>();
     }
 
@@ -43,6 +44,7 @@ public class GUIController : MonoBehaviour
     public void OpenCamera()
     {
         cam.SetActive(!cam.activeSelf);
+        thirdCam.SetActive(!thirdCam.activeSelf);
         TurnOffMenu();
     }
 
---
+++ Assets/ThirdCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdCamera : MonoBehaviour
+{
+    Camera thirdCam;
+    Renderer newRenderer;
+    RenderTexture newT;
+
+    void Start()
+    {
+        thirdCam = Camera.allCameras[0];
+        newRenderer = GetComponent<Renderer>();
+        thirdCam.targetTexture = newT;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newRenderer.material.mainTexture = newT;
+    }
+}
